77399e0a70aad1e938c6a25c709e902314b9ce40
小小酥XXS <syjsu@qq.com>
2016-07-13 13:31:10 +0800

    初始化工程
    初始化工程

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def hello():
+	return "我的第一个SAE容器的Flask案例"
+
+@app.route('/user/<name>')
+def user(name):
+	return "你好,%s" % name
+	
+@app.route('/post/<int:id>')
+def show_post(id):
+    return '您的id号码是%d' % id
+	
+@app.errorhandler(404)
+def not_found(error):
+    return '对不起，您访问的网页找不到 %d' % 404
+	
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=5050)
